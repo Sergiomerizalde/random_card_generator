761af9d2303b3365b9c0de9cfa32e301828facb7
Sergiomerizalde <sergio.merizalde.p@gmail.com>
2022-08-17 20:54:38 +0000

    commit miercoles 4 pm

--- src/app.js
+++ src/app.js
@@ -5,13 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let iconos = ["♦", "♥", "♠", "♣"];
-
 window.onload = function() {
-  suiterandom();
-};
+  let icons = ["♦", "♥", "♠", "♣"];
+  let suite = Math.floor(Math.random() * icons.length);
 
-function suiterandom() {
-  // let suite = Math.floor(Math.random() * iconos.length);
-  // return iconos[suite];
-}
+  let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let randomnumber = Math.floor(Math.random() * number.length);
+
+  let color = icons[suite] === "♥" || icons[suite] === "♠" ? "red" : "black";
+
+  document.querySelector("#addsuite").innerHTML = icons[suite];
+  document.querySelector("#addsuite").style.color = color;
+  document.querySelector("#addsuite2").innerHTML = icons[suite];
+  document.querySelector("#addsuite2").style.color = color;
+  document.querySelector("#addnumber").innerHTML = number[randomnumber];
+};
